22e4db7c6fc820035fcadfecf556b32b62197bc3
Maxime Biloé <maxime.biloe@gmail.com>
2016-07-26 18:36:50 +0200

    // not compatible with PrestaShop 1.7

--- producttooltip.php
+++ producttooltip.php
@@ -42,6 +42,7 @@ class ProductToolTip extends Module
 
 		$this->displayName = $this->l('Product tooltips');
 		$this->description = $this->l('Shows information on a product page: how many people are viewing it, the last time it was sold and the last time it was added to a cart.');
+		$this->ps_versions_compliancy = array('min' => '1.6', 'max' => '1.6.99.99');
 	}
 
 	public function install()
